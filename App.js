62f8a2695db230b5d910414df8df4b6a13b660eb
ibalerobbyjay <ibalerobbyjay@gmail.com>
2025-08-14 11:03:02 +0800

    Color Changer App

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp'; // adjust path if in components folder
+import { SafeAreaView, ScrollView } from 'react-native';
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <CounterApp />
+      <ScrollView style={{ flex: 1 }}>
+        <CounterApp />
+        <ColorChangerApp />
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
 
